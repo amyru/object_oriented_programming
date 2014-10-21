9d80508bcdf0172b489d158b4b0f2fdb0035f8c1
Amy Rudolph <amy.rudolph@live.co.uk>
2014-10-21 14:04:36 -0400

    committing exercise2.rb

---
+++ october21/exercise2.rb
@@ -0,0 +1,15 @@
+#Good tip
+x = 55
+
+y = x * 0.2
+puts "#{y} is a good tip for a #{x} bill amount"
+ 
+
+# string interpolation
+a = 45628
+b = 7839
+
+puts "#{a*b} is the result of multiplying #{a} with #{b}"
+
+# true
+puts (true && false) || (false && true) || !(false && false)
